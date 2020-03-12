69328d4e859b342dc4eaa6edeafff3481b5c6626
关于 <372301467@qq.com>
2020-03-12 15:46:13 +0800

    修复小bug

--- src/glove/common-request.js
+++ src/glove/common-request.js
@@ -1,10 +1,13 @@
 // @ts-check
 const { parseJson, parseQuery, writeJson } = require('../request-util/index')
+const CommonError = require('../common-error')
 
 module.exports = async function(fn, ctx){
   ctx.data = await parseJson(ctx.request)
   ctx.query = parseQuery(ctx.request)
   let result = await fn(ctx)
+  if(result instanceof CommonError) // 自动处理 CommonError
+    result = result.getEntity()
   if(result)
     writeJson(ctx.response, {
       code: 0,
