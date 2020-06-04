168a51762c10bc723011cbc6661bb2d084c69270
关于 <372301467@qq.com>
2020-06-04 20:44:05 +0800

    add new ExpectedError(数据不存在)

--- src/error/expected-error.js
+++ src/error/expected-error.js
@@ -5,6 +5,7 @@ class ExpectedError extends CommonError {}
 ExpectedError.WrongUsernameOrPassword = new CommonError('用户名或密码错误', 2)
 ExpectedError.NotLogin = new CommonError('请先登录', 3)
 ExpectedError.PermissionDenied = new CommonError('没有权限', 4)
+ExpectedError.NotExist = new CommonError('数据不存在', 5)
 
 ExpectedError.NotFound = new CommonError('未找到资源', 404)
 
