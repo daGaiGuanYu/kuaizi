8ed8e7927451fd4b46c59f01adee9524fa9f06c6
关于 <372301467@qq.com>
2020-06-17 13:24:00 +0800

    update

---
+++ test/exception/n-bug/index.js
@@ -0,0 +1,13 @@
+const { Server, HandleRequest, Exception } = require('kuaizi')
+
+Server.start(8080)
+
+HandleRequest.get('/', ctx => {
+  someMethod()
+  console.log('这是执行不到的代码')
+  return '这里也不会返回，因为根本也执行不到这里'
+})
+
+function someMethod(){
+  throw new Exception.NBug('这是返回给用户看的错误信息', 886)
+}
