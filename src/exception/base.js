cbecf81d6b11bd593b0c0cfb49668f861e007c99
关于 <372301467@qq.com>
2020-06-17 11:47:43 +0800

    fix bug in Bug

--- src/exception/base.js
+++ src/exception/base.js
@@ -2,8 +2,7 @@ class Exception extends Error {
   constructor(msg, code = 9999, log){ // NBug 需不需要 log？万一需要统计用户常犯的错误呢
     super(msg)
     this.code = code
-    if(log)
-      console.error()
+    this.log = log
   }
 
   toJSON(){ // 改变 JSON.stringify(new Exception()) 的行为
---
+++ test/exception/bug/index.js
@@ -0,0 +1,13 @@
+const { Server, HandleRequest, Exception } = require('../../../src/index')
+
+Server.start(8080)
+
+HandleRequest.get('/', ctx => {
+  someMethod()
+  console.log('这是执行不到的代码')
+  return '这里也不会返回，因为根本也执行不到这里'
+})
+
+function someMethod(){
+  throw new Exception.Bug('这是返回给用户看的错误信息', 666, '这里记录bug信息')
+}
