c0932941ef7395675e1e64322f8acad437e7ce40
关于 <372301467@qq.com>
2020-06-28 16:35:05 +0800

    更新 example

--- example/handler/custom-response/index.js
+++ example/handler/custom-response/index.js
@@ -1,10 +1,11 @@
-const { HandleRequest, Server } = require('kuaizi')
+const { HandleRequest, Server, Constant } = require('kuaizi')
 
 Server.start(8080)
 
 HandleRequest.get('/', ctx => {
   const serverResponse = ctx.res // 从请求上下文中，拿出响应对象
   serverResponse.write('hell') // 写入 hell
-  serverResponse.write('o') // 写入 o
+  serverResponse.write('o1') // 写入 o
   serverResponse.end() // 标记响应完成～
+  return Constant.Nothing
 })
